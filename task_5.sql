0467d20ea96a1c109f56b9855d3f3aeaab3a2746
mahmoudeladl212 <mahmoudeladl212@outlook.com>
2025-06-29 22:04:23 +0300

    db 7 1 submmision

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,6 @@
--- task_5.sql - Insert single customer record
+-- task_6.sql - Insert multiple customer records
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+VALUES 
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemiah Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
