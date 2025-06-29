723237164630640b6c3b1968f77aef25a5280819
mahmoudeladl212 <mahmoudeladl212@outlook.com>
2025-06-29 21:46:13 +0300

    db 5 submmision

---
+++ task_4.sql
@@ -0,0 +1,13 @@
+-- task_4.sql - Prints full description of books table
+SELECT 
+    COLUMN_NAME AS 'Field',
+    COLUMN_TYPE AS 'Type',
+    IS_NULLABLE AS 'Null',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS 'Extra'
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = DATABASE() 
+    AND TABLE_NAME = 'books';
